d8dbf3addd114467d5fa3c393f2958d42ab413e5
Kevin Nguyen Vo <nguyen.vo@oculus.com>
2021-04-03 14:41:51 -0700

    Add +-*/ features

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,6 +1,70 @@
 import React, { Component } from 'react'
 
 export default class Calculator extends Component {
+    state = {
+        result: 0,
+        currentValue: "0",
+        displayScreenValue: "0",
+        lastOperator: "+",
+        opDuplicatingCount: 0
+    }
+
+    keyPressHandler = (buttonValue) => {
+        console.log(buttonValue);
+        // @=CE $=C
+        if ("+-*÷=".indexOf(buttonValue) === -1) {
+            if (this.state.currentValue === "0") {
+                this.setState({
+                    currentValue: buttonValue,
+                    displayScreenValue: buttonValue
+                });
+            } else {
+                this.setState({
+                    currentValue: this.state.currentValue + buttonValue,
+                    displayScreenValue: this.state.currentValue + buttonValue
+                })
+            }
+            this.setState({
+                opDuplicatingCount: 0
+            })
+        } else {
+            if (this.state.opDuplicatingCount < 1) {
+                let tempResult;
+                if ("+-*÷=".indexOf(this.state.lastOperator) !== -1) {
+                    switch (this.state.lastOperator) {
+                        case "+":
+                            tempResult = parseFloat(this.state.result) + parseFloat(this.state.currentValue);
+                            break;
+                        case "-":
+                            tempResult = parseFloat(this.state.result) - parseFloat(this.state.currentValue);
+                            break;
+                        case "*":
+                            tempResult = parseFloat(this.state.result) * parseFloat(this.state.currentValue);
+                            break;
+                        default:
+                            tempResult = parseFloat(this.state.result) / parseFloat(this.state.currentValue);
+                    }
+                    this.setState({
+                        currentValue: "0",
+                        result: tempResult,
+                        displayScreenValue: tempResult
+                    })
+                }
+
+                this.setState({
+                    opDuplicatingCount: this.state.opDuplicatingCount + 1
+                })
+            }
+            this.setState({
+                lastOperator: buttonValue
+            })
+
+        }
+    }
+
+    componentDidUpdate = () => {
+        console.log(JSON.stringify(this.state));
+    }
 
 
     render() {
@@ -10,8 +74,9 @@ export default class Calculator extends Component {
             position: 'absolute',
             top: '50%',
             left: '50%',
-            transform: 'translate(-50%, -50%)',
+            transform: 'translate(-50%, -80%)',
             width: '400px',
+            backgroundColor: "pink",
         };
 
         const styleScreen = {
@@ -28,7 +93,7 @@ export default class Calculator extends Component {
 
         const styleBtn = {
             height: "60px",
-            fontSize: "2rem!important",
+            fontSize: "2rem !important",
         };
 
         const styleCalculatorKeys = {
@@ -48,40 +113,34 @@ export default class Calculator extends Component {
         }
 
         return (
-            <div className="container-fluid bg-danger">
-                <div className="container">
-                    <h1 className="display-4">Calculator</h1>
-                    <div className="jumbotron-fluid calculator card" style={styleCalculator}>
-                        <input type="text" class="calculator-screen z-depth-1 text-right" value="0" disabled style={styleScreen} />
-                        <div className="calculator-keys" style={styleCalculatorKeys}>
-                            <button type="button" style={styleBtn} value={7} className="btn btn-light waves-effect">7</button>
-                            <button type="button" style={styleBtn} value={8} className="btn btn-light waves-effect">8</button>
-                            <button type="button" style={styleBtn} value={9} className="btn btn-light waves-effect">9</button>
-                            <button type="button" style={styleBtn} className="operator btn btn-info" value="/">/</button>
-                            <button type="button" style={styleBtn} className="all-clear function btn btn-danger btn-sm" value="all-clear">CE</button>
+            <div className="calculator card" style={styleCalculator}>
+                <input type="text" className="calculator-screen z-depth-1 text-right" value={this.state.displayScreenValue} disabled style={styleScreen} />
+                    <div className="calculator-keys" style={styleCalculatorKeys}>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="7" className="btn btn-light waves-effect">7</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="8" className="btn btn-light waves-effect">8</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="9" className="btn btn-light waves-effect">9</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="operator btn btn-info" value="÷">/</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="all-clear function btn btn-danger btn-sm" value="@">CE</button>
 
-                            <button type="button" style={styleBtn} value={4} className="btn btn-light waves-effect">4</button>
-                            <button type="button" style={styleBtn} value={5} className="btn btn-light waves-effect">5</button>
-                            <button type="button" style={styleBtn} value={6} className="btn btn-light waves-effect">6</button>
-                            <button type="button" style={styleBtn} className="operator btn btn-info" value="*">×</button>
-                            <button type="button" style={styleBtn} className="all-clear function btn btn-danger btn-sm" value="all-clear">C</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="4" className="btn btn-light waves-effect">4</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="5" className="btn btn-light waves-effect">5</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="6" className="btn btn-light waves-effect">6</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="operator btn btn-info" value="*">×</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="all-clear function btn btn-danger btn-sm" value="$">C</button>
 
-                            <button type="button" style={styleBtn} value={1} className="btn btn-light waves-effect">1</button>
-                            <button type="button" style={styleBtn} value={2} className="btn btn-light waves-effect">2</button>
-                            <button type="button" style={styleBtn} value={3} className="btn btn-light waves-effect">3</button>
-                            <button type="button" style={styleBtn} className="operator btn btn-info" value="-">-</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="1" className="btn btn-light waves-effect">1</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="2" className="btn btn-light waves-effect">2</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} value="3" className="btn btn-light waves-effect">3</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="operator btn btn-info" value="-">-</button>
 
-                            <button type="button" style={styleZeroBtn} value={0} className="btn btn-light waves-effect">0</button>
-                            <button type="button" style={styleBtn} className="decimal function btn btn-secondary" value=".">.</button>
-                            <button type="button" style={styleBtn} className="operator btn btn-info" value="+">+</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleZeroBtn} value="0" className="btn btn-light waves-effect">0</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="decimal function btn btn-secondary" value=".">.</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleBtn} className="operator btn btn-info" value="+">+</button>
 
-                            <button type="button" style={styleEqualSign} className="equal-sign operator btn btn-primary" value="=">=</button>
+                    <button type="button" onClick={(e) => this.keyPressHandler(e.target.value)} style={styleEqualSign} className="equal-sign operator btn btn-primary" value="=">=</button>
 
-                        </div>
                     </div>
-                </div>
-            </div >
-
+            </div>
         )
     }
 }
